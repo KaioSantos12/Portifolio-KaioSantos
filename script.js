c4ff5fc990dc4500fc9727c8faf0f6a8e7e1331b
kaiosantos7 <129885729+KaioSantos12@users.noreply.github.com>
2026-03-12 02:44:37 -0300

    Criação da tela inicial e estilização usando css e js para animações, fundo, textos e informações criadas

---
+++ script.js
@@ -0,0 +1,43 @@
+tsParticles.load("particles-js", {
+  background: {
+    color: "#131313"
+  },
+  particles: {
+    number: {
+      value: 25
+    },
+    color: {
+      value: "#00bfff"
+    },
+    opacity: {
+      value: 0.4
+    },
+    size: {
+      value: 2
+    },
+    move: {
+      enable: true,
+      speed: 1
+    },
+    links: {
+      enable: true,
+      distance: 150,
+      color: "#00bfff",
+      opacity: 0.2,
+      width: 1
+    }
+  }
+});
+
+const texto = "Desenvolvedor ";
+let i = 0;
+
+function escrever(){
+    if(i < texto.length){
+        document.getElementById("typing").innerHTML += texto.charAt(i);
+        i++;
+        setTimeout(escrever, 100);
+    }
+}
+
+escrever();
